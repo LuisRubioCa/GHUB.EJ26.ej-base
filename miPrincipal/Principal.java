543ec9cf30781d1491fc33f031aae93b24e95d63
Rubio Cardenas Luis Angel <L25171057@culiacan.tecnm.mx>
2026-02-11 17:58:34 +0000

    clase punto

--- miPrincipal/Principal.java
+++ miPrincipal/Principal.java
@@ -1,11 +1,27 @@
 package miPrincipal;
 
 public class Principal {
-    public String getGreeting() {
-        return "Hello World!";
-    }
 
     public static void main(String[] args) {
-        System.out.println(new Principal().getGreeting());
+       // System.out.println("Hola mundo");
+       punto p1 = new punto();
+       punto p2 = new punto(1,1);
+       punto p3 = new punto(4,5);
+       p1.fijarX(3);
+       p1.fijarY(4);
+
+       System.out.print("x ="+p1.leerX()+"  ");
+       System.out.println("y ="+p1.leerY());
+
+       System.out.print("x ="+p2.leerX()+"  ");
+       System.out.println("y ="+p2.leerY());
+
+       p3.fijarY(4);
+       
+       System.out.print("x ="+p3.leerX()+"  ");
+       System.out.println("y ="+p3.leerY());
+
+
+
     }
 }
---
+++ miPrincipal/punto.java
@@ -0,0 +1,31 @@
+package miPrincipal;
+
+public class punto {
+    //Atributos
+    private int x; //coordenada x
+    private int y; //coordenada y
+    //metodos contructores
+    public punto(){
+        x = 0;
+        y = 0;
+
+    }
+    public punto(int x_, int y_){
+        x = x_;
+        y = y_;
+
+    }
+    public void fijarX(int valorX){
+        x = valorX;
+
+    }
+    public void fijarY(int valorY){
+        y = valorY;
+    }
+    public int leerX(){
+        return x;
+    }
+    public int leerY(){
+        return y;
+    }
+}
